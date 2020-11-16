0d43d00feca55903b0de5c1ebf3d0f4da91daed6
aulia-rosyida <rosyidaaulia24@gmail.com>
2020-11-16 08:59:34 +0700

    first init

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.aulia_rosyida.dietskuy.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
