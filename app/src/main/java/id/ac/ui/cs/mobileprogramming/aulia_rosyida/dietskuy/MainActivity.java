4051dff942540bac973371a614f64533dcdba5ce
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-01-03 10:19:47 +0700

    workout activity

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -14,6 +15,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.Locale;
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         //Change action bar title, if you dont change it will be according to your system default language
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(getResources().getString(R.string.app_name));
+
     }
 
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/ui/main/MainFragment.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/ui/main/MainFragment.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.mobileprogramming.aulia_rosyida.dietskuy.ui.main;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import id.ac.ui.cs.mobileprogramming.aulia_rosyida.dietskuy.R;
+import id.ac.ui.cs.mobileprogramming.aulia_rosyida.dietskuy.WorkoutActivity;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    Button b1;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -26,7 +30,16 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        View v = inflater.inflate(R.layout.main_fragment, container, false);
+
+        b1 = v.findViewById(R.id.workout_button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), WorkoutActivity.class));
+            }
+        });
+        return v;
     }
 
     @Override
