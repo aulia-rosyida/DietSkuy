e70b925549cd7c3e8a12d935ea44400e397dbd54
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-01-30 20:36:29 +0700

    make openGLRenderer

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/OpenGLRenderer.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/aulia_rosyida/dietskuy/OpenGLRenderer.java
@@ -1,24 +1,29 @@
-// package id.ac.ui.cs.mobileprogramming.aulia_rosyida.dietskuy;
+ package id.ac.ui.cs.mobileprogramming.aulia_rosyida.dietskuy;
 
-// import android.opengl.GLES20;
-// import android.opengl.GLSurfaceView;
+ import android.opengl.GLES20;
+ import android.opengl.GLSurfaceView;
 
-// import javax.microedition.khronos.egl.EGLConfig;
-// import javax.microedition.khronos.opengles.GL10;
+ import javax.microedition.khronos.egl.EGLConfig;
+ import javax.microedition.khronos.opengles.GL10;
 
-// public class OpenGLRenderer implements GLSurfaceView.Renderer {
-//     @Override
-//     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-//         GLES20.glClearColor(1f, 0, 0,1f);
-//     }
+ public class OpenGLRenderer implements GLSurfaceView.Renderer {
+     private double redValue = 1f;
+     private static final double FLASH_DURATION = 1000;
+     @Override
+     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+         GLES20.glClearColor((float)redValue, 0, 0,1f);
+     }
 
-//     @Override
-//     public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+     @Override
+     public void onSurfaceChanged(GL10 gl10, int i, int i1) {
 
-//     }
+     }
 
-//     @Override
-//     public void onDrawFrame(GL10 gl10) {
-//         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-//     }
-// }
+     @Override
+     public void onDrawFrame(GL10 gl10) {
+         GLES20.glClearColor((float) redValue, 0,0, 1f);
+         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+         redValue = ((Math.sin(System.currentTimeMillis() * 2 * Math.PI / FLASH_DURATION) * 0.5) + 0.5);
+     }
+ }
